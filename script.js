f62d3726493abe02a77b755caf4a7c38b93d5509
radhhh <123277320+radhhh@users.noreply.github.com>
2023-07-09 18:06:57 +0700

    Finished Single Player Mode

--- script.js
+++ script.js
@@ -5,6 +5,58 @@ function Player(symbol){
 
 const player = [new Player('X'), new Player('O')];
 
+const robot = (() => {
+    const validTrack = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+                    [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+    let difficulty = 1;
+    const setDifficulty = function(selectedDifficulty){
+        difficulty = selectedDifficulty;
+    }
+    const easyBot = function(){
+        const boardState = board.getState();
+        const boardAvailableState = boardState
+            .map((value, index) => {
+                return {val: value, idx: index}})
+            .filter((current) => isNaN(current.val));
+        return boardAvailableState[parseInt(Math.random()*boardAvailableState.length)].idx;
+    }
+    const mediumBot = function(){
+        const boardState = board.getState();
+        let nextMove;
+        validTrack.forEach((track) => {
+            const status = track.map((value) => boardState[value]);
+            if((status[1] == status[2]) && isNaN(status[0])){
+                nextMove = track[0];
+                if(status[1] == 1) return;
+            }
+            if((status[0] == status[2]) && isNaN(status[1])){
+                nextMove = track[1];
+                if(status[0] == 1) return;
+            }
+            if((status[0] == status[1]) && isNaN(status[2])){
+                nextMove = track[2];
+                if(status[2] == 1) return;
+            }
+        });
+        if(nextMove != undefined) return nextMove;
+        else return easyBot();
+    }
+    const move = function(){
+        switch(difficulty){
+            case 0:
+                controller.movePlayer(easyBot());
+                break;
+            case 1:
+                controller.movePlayer(mediumBot());
+                break;
+            case 2:
+                controller.movePlayer(hardBot());
+                break;
+        }
+    }
+    return {move, setDifficulty};
+})();
+
 const display = (() => {
     const turnDisplay = document.querySelector('#turnDisplay'),
     gridContent = document.querySelectorAll('.gridContent'),
@@ -14,6 +66,7 @@ const display = (() => {
     tieDisplay = document.querySelector('#displayTie');
     overlayDark = document.querySelector('#overlayDark'),
     newRoundWindow = document.querySelector('#newRoundWindow'),
+    modeSelectorWindow = document.querySelector('#modeSelectorWindow');
     winnerText = document.querySelector('#winnerText');
 
     const addPreview = function(playerID, location){
@@ -34,7 +87,7 @@ const display = (() => {
     }
     const updateScore = function(){
         xDisplay.textContent = player[0].score;
-        tieDisplay.textContent = controller.tieCount;
+        tieDisplay.textContent = controller.getTieCount();
         oDisplay.textContent = player[1].score;
     }
     const showWinGrid = function(winningLine){
@@ -47,13 +100,21 @@ const display = (() => {
         }
     }
     const showNewRound = function(winningPlayer){
-        if(winningPlayer) winnerText.textContent = `Player ${player[winningPlayer].symbol} is the winner`;
+        if(winningPlayer !== undefined) winnerText.textContent = `Player ${player[winningPlayer].symbol} is the winner!`;
         else winnerText.textContent = 'The game is tie'
         newRoundWindow.classList.remove('hidden');
         overlayDark.classList.remove('hidden');
     };
     const hideNewRound = function(){
         newRoundWindow.classList.add('hidden');
+        overlayDark.classList.add('hidden');
+    }
+    const showModeSelector = function(){
+        modeSelectorWindow.classList.remove('hidden');
+        overlayDark.classList.remove('hidden');
+    }
+    const hideModeSelector = function(){
+        modeSelectorWindow.classList.add('hidden');
         overlayDark.classList.add('hidden');
     }
     const resetGrid = function(){
@@ -63,7 +124,7 @@ const display = (() => {
             gridContent[i].textContent = "";
         }
     }
-    return {addPreview, removePreview, showMove, updateTurn, updateScore, showWinGrid, showNewRound, hideNewRound, resetGrid};
+    return {addPreview, removePreview, showMove, updateTurn, updateScore, showWinGrid, showNewRound, hideNewRound, showModeSelector, hideModeSelector, resetGrid};
 })();
 
 const board = (() => {
@@ -71,6 +132,9 @@ const board = (() => {
     let state = defaultState;
     const validTrack = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
                         [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+    const getState = function(){
+        return state;
+    }
     const checkWinning = function(){
         let value;
         validTrack.forEach((track) => {
@@ -92,28 +156,38 @@ const board = (() => {
         state = defaultState.slice();
         console.log(state);
     }
-    return {isValid, movePlayer, checkWinning, reset};
+    return {isValid, getState, movePlayer, checkWinning, reset};
 })();
 
 const controller = (() => {
-    let round, active = true, tieCount = 0, move = 0;
+    let round, active = false, tieCount = 0, move, gameMode; 
+    // gameMode = 0 (multiplayer), 1 (single easy), 2 (single hard)
     let currentPlayer;
     const isActive = function(){return active};
     const activate = function(){
         active = true
     };
+    const getTieCount = function(){
+        return tieCount;
+    }
     const nextTurn = function(){
         currentPlayer = currentPlayer == 0 ? 1 : 0;
         display.updateTurn(currentPlayer);
+        if(gameMode == 'single' && currentPlayer == 1){
+            active = false;
+            setTimeout(() => {active = true; robot.move();}, 1000);
+        }
     }
     const resetScore = function(){
         player[0].score = player[1].score = 0;
+        tieCount = 0;
     }
     const winGame = function(result){
         display.showWinGrid(result);
         active = false;
         player[currentPlayer].score++;
         display.updateScore();
+        console.log('winGame', currentPlayer);
         setTimeout(() => {
             display.showNewRound(currentPlayer);
         }, 1000);
@@ -121,10 +195,11 @@ const controller = (() => {
     const tieGame = function(){
         active = false;
         tieCount++;
-        display.updateScore();
+        display.updateScore(); 
+        console.log('tieGame');
         setTimeout(() => {
             display.showNewRound();
-        })
+        }, 1000);
     }
     const movePlayer = function(location){
         board.movePlayer(currentPlayer, location);
@@ -135,39 +210,61 @@ const controller = (() => {
         else if(move == 9) tieGame();
         else nextTurn();
     };
-    const newGame = function(){
-        round = 1;
-        move = 0;
-        currentPlayer = 0;
-        display.updateTurn(currentPlayer);
+    const newGame = function(selectedMode){
         resetScore();
         display.updateScore();
-        display.resetGrid();
-        board.reset();
+        gameMode = selectedMode;
+        round = 0;
+        nextRound();
     };
     const nextRound = function(){
+        display.resetGrid();
+        board.reset();
         round++;
         move = 0;
         currentPlayer = (round % 2 == 1) ? 0 : 1;
         display.updateTurn(currentPlayer);
-        display.resetGrid();
-        board.reset();
+        if(gameMode == 'single' && currentPlayer == 1){
+            active = false;
+            setTimeout(() => {active = true; robot.move();}, 1000);
+        }
     }
-    return {activate, isActive, tieCount, newGame, movePlayer, newGame, nextRound};
+    return {activate, isActive, getTieCount, newGame, movePlayer, newGame, nextRound, resetScore};
 })();
 
 function handleClick(location){
     if(board.isValid(location) && controller.isActive()) controller.movePlayer(location);
 }
+
 function handleNewGame(){
     display.hideNewRound();
-    controller.activate();
-    controller.newGame();
+    controller.resetScore();
+    display.updateScore();
+    display.resetGrid();
+    board.reset();
+    display.updateTurn(0);
+    setTimeout(() => display.showModeSelector(), 500);
 }
+
 function handleNextRound(){
     display.hideNewRound();
     controller.activate();
     controller.nextRound();
 }
 
-controller.newGame();
+function handleSelectMode(selectedMode){
+    display.hideModeSelector();
+    controller.activate();
+    switch(selectedMode){
+        case 'singleEasy':
+            robot.setDifficulty(0)
+            controller.newGame('single');
+            break;
+        case 'singleMedium':
+            robot.setDifficulty(1);
+            controller.newGame('single');
+            break;
+        case 'multiplayer':
+            controller.newGame('multi');
+    }
+}
